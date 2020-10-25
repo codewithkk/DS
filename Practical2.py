6d51e58bac2fce0fdd1980885eedaa841b364db5
Kamlesh Kanojia <61698357+codewithkk@users.noreply.github.com>
2020-10-25 17:55:49 +0530

    Update Practical2.py

--- Practical2.py
+++ Practical2.py
@@ -1,84 +1,108 @@
-def _init_(self, data): 
+class Stack():
+    def __init__(self):
+        self.items = ['4','3','2','1','Kamlesh','abc']
 
-        self.data = data 
+    def end(self, item):
+        self.items.append(item)
+        print(item)
 
-        self.next = None
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            return None
 
-class LinkedList: 
+    def size(self):
+        if self.items:
+            return len(self.items)
+        else:
+            return None
 
-    # Function to initialize head 
+    def display(self):
+        for i in self.items:
+            print(i)
 
-    def _init_(self): 
+    def start(self, i):
+        self.items.insert(0, i)
 
-        self.head = None
+    def search(self, a):
+        l = self.items
+        for i in l:
+            if i == a:
+                print("found Value : ", a)
+                break
+        else:
+            print("not found")
 
- 
-    # Function to reverse the linked list 
+    def traverse(self):
+        a = []
+        l = self.items
+        for i in l:
+            a.append(i)
+        print(a)
+    def shoting_element(self):
+        #bubble shotting
+        nums=self.items
+        def sort(nums):
+            for i in range(len(nums) - 1, 0, -1):
+                for j in range(i):
+                    if nums[j] > nums[j + 1]:
+                        temp = nums[j]
+                        nums[j] = nums[j + 1]
+                        nums[j + 1] = temp
 
-    def reverse(self): 
+        sort(nums)
+        print(nums)
+    #reverse
+    def reverse(self):
+        l=self.items
+        print(l[::-1])
 
-        prev = None
+    def remove_value_from_particular_index(self,a):
+        l=self.items
+        l.pop(a)
+        print(l)
 
-        current = self.head 
+class merge1(Stack):
+    #inheritance
+    def __init__(self):
+        Stack.__init__(self)
+        self.items1 = ['4','3','2','1','6']
 
-        while(current is not None): 
+    def merge(self):
+        l = self.items
+        l1=self.items1
+        a=(l+l1)
+        a.sort()
+        print(a)
 
-            next = current.next
+s = Stack()
+# Inserting the values
+s.end('-1')
+s.start('-2')
+s.start('5')
+s.end('6')
+s.end('7')
+s.start('-1')
+s.start('-2')
+print("search the specific value : ")
+s.search('-2')
 
-            current.next = prev 
+print("Display the values one by one :")
+s.display()
+print("peek (End  Value) :", s.peek())
+print("treverse the values : ")
+s.traverse()
+#Shotting element
+print("Shotting the values : ")
+s.shoting_element()
+#reversing the list
+print("Reversing  the values : ")
+s.reverse()
 
-            prev = current 
+print("remove value from particular index which is defined earlier")
+s.remove_value_from_particular_index(0)
 
-            current = next
-
-        self.head = prev 
-
-          
-
-    # Function to insert a new node at the beginning 
-
-    def push(self, new_data): 
-
-        new_node = Node(new_data) 
-
-        new_node.next = self.head 
-
-        self.head = new_node 
-
-  
-
-    # Utility function to print the linked LinkedList 
-
-    def printList(self): 
-
-        temp = self.head 
-
-        while(temp): 
-
-            print temp.data, 
-
-            temp = temp.next
-
-  
-
-  
-# Driver program to test above functions 
-
-llist = LinkedList() 
-
-llist.push(20) 
-
-llist.push(4) 
-
-llist.push(15) 
-
-llist.push(85) 
-
-  
-
-print "Given Linked List"
-llist.printList() 
-llist.reverse() 
-
-print "\nReversed Linked List"
-llist.printList()
+s1=merge1()
+print("merge")
+s1.merge()
